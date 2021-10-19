9ce4936662c0c0d70382c6a152db15ecde4ea986
vuong quang huy <62917858+vqh2602@users.noreply.github.com>
2021-10-19 17:15:07 +0700

    git add

--- app/src/main/java/com/example/vqhcovid/HomeActivity.java
+++ app/src/main/java/com/example/vqhcovid/HomeActivity.java
@@ -11,6 +11,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -53,14 +54,25 @@ import com.example.vqhcovid.weather.Weather24;
 import com.example.vqhcovid.weather.Weather7;
 import com.google.android.material.navigation.NavigationView;
 import com.huawei.hmf.tasks.OnCompleteListener;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
 import com.huawei.hmf.tasks.Task;
 import com.huawei.hms.common.ApiException;
+import com.huawei.hms.common.ResolvableApiException;
 import com.huawei.hms.kit.awareness.Awareness;
 import com.huawei.hms.kit.awareness.status.WeatherStatus;
 import com.huawei.hms.kit.awareness.status.weather.DailyWeather;
 import com.huawei.hms.kit.awareness.status.weather.HourlyWeather;
 import com.huawei.hms.kit.awareness.status.weather.Situation;
 import com.huawei.hms.kit.awareness.status.weather.WeatherSituation;
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.LocationRequest;
+import com.huawei.hms.location.LocationServices;
+import com.huawei.hms.location.LocationSettingsRequest;
+import com.huawei.hms.location.LocationSettingsResponse;
+import com.huawei.hms.location.LocationSettingsStates;
+import com.huawei.hms.location.LocationSettingsStatusCodes;
+import com.huawei.hms.location.SettingsClient;
 import com.huawei.hms.push.HmsMessaging;
 import com.huawei.hms.support.account.AccountAuthManager;
 import com.huawei.hms.support.account.request.AccountAuthParams;
@@ -109,10 +121,21 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     Handler mHandler;
     private MyReceiver receiver;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    // Define a location request object.
+    private LocationRequest mLocationRequest;
+    SettingsClient settingsClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // lây vi tri hms
+        AssigningAppPermissions();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
 
         bindViewId();
 
@@ -165,10 +188,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         if (isAllPermissionAllowed()) {
             // You can use the API that requires the permission.
             startGetWeathers();
+
         } else if (shouldShowRequestPermissionRationale(accessFineLocation)) {
            //TODO show UI to ask permission again
-        } else if (shouldShowRequestPermissionRationale(accessCoarseLocation)){
+            checkLocationSeting();
+        }
+        else if (shouldShowRequestPermissionRationale(accessCoarseLocation)){
             //TODO show UI to ask permission again
+            checkLocationSeting();
         }
         else {
             ActivityCompat.requestPermissions(
@@ -994,5 +1021,77 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+// check vị trí Assigning App Permissions
+    private void AssigningAppPermissions(){
+        // Dynamically apply for required permissions if the API level is 28 or lower.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            Log.i(TAG, "android sdk <= 28 Q");
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(this, strings, 1);
+            }
+        } else {
+            // Dynamically apply for the android.permission.ACCESS_BACKGROUND_LOCATION permission in addition to the preceding permissions if the API level is higher than 28.
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED) {
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(this, strings, 2);
+            }
+        }
+    }
 
+    //
+    private void checkLocationSeting(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        mLocationRequest = new LocationRequest();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+// Check the device location settings.
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                // Define the listener for success in calling the API for checking device location settings.
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        LocationSettingsStates locationSettingsStates =
+                                locationSettingsResponse.getLocationSettingsStates();
+                        StringBuilder stringBuilder = new StringBuilder();
+                        // Checks whether the location function is enabled.
+                        stringBuilder.append(",\nisLocationUsable=")
+                                .append(locationSettingsStates.isLocationUsable());
+                        // Checks whether HMS Core (APK) is available.
+                        stringBuilder.append(",\nisHMSLocationUsable=")
+                                .append(locationSettingsStates.isHMSLocationUsable());
+                        Log.i(TAG, "checkLocationSetting onComplete:" + stringBuilder.toString());
+                    }
+                })
+                // Define callback for failure in checking the device location settings.
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        // Processing when the device is a Huawei device and has HMS Core (APK) installed, but its settings do not meet the location requirements.
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    // Call startResolutionForResult to display a popup asking the user to enable related permission.
+                                    rae.startResolutionForResult(HomeActivity.this, 0);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    // TODO
+                                }
+                                break;
+                        }
+                    }
+                });
+    }
 }
